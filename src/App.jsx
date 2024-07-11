ac585f8fde38cb41fe94bcfc24aba6cdc6b2bb22
priyanshu-nickelfox <priyanshu.singh@nickelfox.com>
2024-07-12 00:38:21 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Application from "./components/Application/Application";
 import Benefits from "./components/Benefits/Benefits";
 import Collaboration from "./components/Collaboration/Collaboration";
 import Enterprise from "./components/Enterprise/Enterprise";
-import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import IndustrySolutions from "./components/IndustrySolutions/IndustrySolutions";
 import WhatWeOffer from "./components/WhatWeOffer/WhatWeOffer";
